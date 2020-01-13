285ed1352a9d30e7f0c65d1d9a162587d85b3d66
Joshua O'bannon <jtobannon@gmail.com>
2020-01-13 09:55:46 -0700

    Remove comments from show spec

--- spec/features/flights/show_spec.rb
+++ spec/features/flights/show_spec.rb
@@ -1,13 +1,3 @@
-# As a visitor
-# When I visit a flights show page ('/flights/:id')
-# I see all of that flights information including:
-#   - number
-#   - date
-#   - time
-#   - departure city
-#   - arrival city
-# And I see the name of the airline this flight belongs
-# And I see the names of all of the passengers on this flight
 require 'rails_helper'
 
 RSpec.describe "Flight Show Spec" do
